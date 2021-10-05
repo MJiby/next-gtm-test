dc918b365d0a44c5782833c6410290f13ff89cc2
Mycelebs <mycelebs_151@Mycelebsui-Macmini-2.local>
2021-10-05 14:16:10 +0900

    update

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet" />
                     {/* <!-- Google Tag Manager --> */}
                     <script
                         dangerouslySetInnerHTML={{
